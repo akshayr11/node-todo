8241128fdf430b4bca035352dc82194f4ddfc22a
Akshay Rajput <akshay.rajput@coditas.com>
2018-11-22 15:42:59 +0530

    added script to update todos

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { ObjectId } = require("mongodb");
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./model");
+const { pick, isBoolean } = require("lodash");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,6 +22,7 @@ app.post("/todos", (req, res) => {
 		}
 	);
 });
+
 // get all todos
 app.get("/todos", (req, res) => {
 	Todo.find().then(
@@ -67,6 +69,36 @@ app.delete("/todos/:id", (req, res) => {
 		}
 	);
 });
+
+// update Route
+app.patch("/todos/:id", (req, res) => {
+	const id = req.params.id;
+	const body = pick(req.body, ["text", "completed"]);
+	if (isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+	Todo.findByIdAndUpdate(
+		id,
+		{
+			$set: body
+		},
+		{ new: true }
+	).then(
+		todo => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			res.send(todo);
+		},
+		e => {
+			res.status(400).send();
+		}
+	);
+});
+
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
 });
